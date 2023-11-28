b0cc93a003e42c88b47e21313752d44226cbf8d2
Mayurjain2002 <mayurj582002@gmail.com>
2023-11-28 22:29:04 +0530

    mayur

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <button
           className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'
           onClick={() => {
-            window.open("https://wa.me/+919425600513");
+            window.open("https://wa.me/+918602119424");
           }}  >Contact Me</button>
           
           
